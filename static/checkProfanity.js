ab59a1e791c53685496a4343446230131ae63305
Kelvin Kijanda <67392007+K-Kelvin@users.noreply.github.com>
2023-03-09 12:00:52 +0000

    Addded profanity checking

--- static/checkProfanity.js
+++ static/checkProfanity.js
@@ -1,48 +1,22 @@
-let badWords = [];
+function checkProfanity(text) {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("content", text);
+    encodedParams.append("censor-character", "*");
 
-async function populateBadWords() {
-    try {
-        const response = await fetch(
-            "https://docs.google.com/spreadsheets/d/1hIEi2YG3ydav1E06Bzf2mQbGZ12kh2fe4ISgLg_UBuM/export?format=csv"
-        );
-        // const data = await response.json()
-        console.log(response)
-        // const lines = data.split("\n");
-        // badWords = lines.map((line) => line.split(",")[0]);
-        // console.log(badWords)
-        // return badWords;
-    } catch (error) {
-        console.error(error);
-    }
-}
-populateBadWords();
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': '50165337b5msh2a6235f7d2415d8p1604d2jsn59a69a110664',
+            'X-RapidAPI-Host': 'neutrinoapi-bad-word-filter.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
 
-function cussWordFound(input) {
-    if (!input) {
-        return false;
-    }
-
-    let input = input
-        .replace(/1/g, "i")
-        .replace(/!/g, "i")
-        .replace(/3/g, "e")
-        .replace(/4/g, "a")
-        .replace(/@/g, "a")
-        .replace(/5/g, "s")
-        .replace(/7/g, "t")
-        .replace(/0/g, "o")
-        .replace(/9/g, "g");
-
-    const inputWords = input.toLowerCase().split(/[^a-zA-Z]/g);
-    for (const inputWord of inputWords) {
-        if (badWords.includes(inputWord)) {
-            foundProfanity = true;
-            console.log(`${inputWord} is a bad word`);
-            alert(`Use of words like ${input} is not allowed on this platform!`)
-            return true;
-        }
-    }
-    foundProfanity = false;
-    console.log("Clean message.........")
-    return false;
+    return new Promise((resolve, reject) => {
+        fetch('https://neutrinoapi-bad-word-filter.p.rapidapi.com/bad-word-filter', options)
+        .then(response => response.json())
+        .then(response => resolve(response))
+        .catch(err => reject(err));
+    })
 }
