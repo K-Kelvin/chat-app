edd3e9b57037367e2c1e1f32f3e46456550691a6
Kelvin Kijanda <67392007+K-Kelvin@users.noreply.github.com>
2023-03-09 10:05:38 +0000

    latest changes

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def index(request):
     ''' Initial page of the chat app'''
-    return render(request, "chat/index.html")
+    users = User.objects.all().exclude(username=request.user) # other users except the current user
+    return render(request, "chat/chat.html", {
+        "users": users,
+    })
 
 def room(request, room_name):
     ''' Render chat room UI and logic'''
-    return render(request, "chat/chat.html", {"room_name": room_name})
+    users = User.objects.all().exclude(username=request.user) # other users except the current user
+    return render(request, "chat/chat.html", {
+        "room_name": room_name,
+        "users": users,
+    })
