ecc0fb32c2ef4e353011f72aa42b707f520ddb0c
Nivlek <kelvinkijanda@gmail.com>
2023-03-10 04:54:04 +0300

    Added momentjs formatting, set USE_TZ to False

--- static/helpers.js
+++ static/helpers.js
@@ -1,13 +1,15 @@
 function constructMessage(message, sent) {
     const msgDom = document.createElement("div");
+    const date = moment().format('hh:mm A | Do MMM');
     if (sent) {
-        msgDom.className = "d-flex flex-row justify-content-end mb-4";
+        msgDom.className = "d-flex flex-row justify-content-end mb-2";
         msgDom.style.maxWidth = "60%";
         msgDom.style.marginLeft = "auto";
         msgDom.innerHTML = `<div>
 						<p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
 							${message}
 						</p>
+                        <p class="small me-3 mb-3 rounded-3 text-muted">${date}</p>
 						</div>
 						<img
 							src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
@@ -15,7 +17,7 @@ function constructMessage(message, sent) {
 							style="width: 45px; height: 100%"
 						/>`
     } else {
-        msgDom.className = "d-flex flex-row justify-content-start mb-4";
+        msgDom.className = "d-flex flex-row justify-content-start mb-2";
         msgDom.style.maxWidth = "60%";
         msgDom.innerHTML = `<img
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
@@ -29,6 +31,7 @@ function constructMessage(message, sent) {
                 >
                     ${message}
                 </p>
+                <p class="small ms-3 mb-3 rounded-3 text-muted float-end">${date}</p>
             </div>`
     }
     return msgDom;
